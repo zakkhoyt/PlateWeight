c663c151e38df09f8e5436cf97d20d2a3617e5c5
Zakk Hoyt <vaporwarewolf@gmail.com>
2015-10-01 16:59:25 -0700

    Commmitting before adding watch app

--- PlateWeight/ZHKilogramsTableViewController.swift
+++ PlateWeight/ZHKilogramsTableViewController.swift
@@ -12,21 +12,8 @@ class ZHKilogramsTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    }
-
-    override func prefersStatusBarHidden() -> Bool {
-        return true
-    }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.navigationItem.title = "kgs to lbs";
     }
 
     // MARK: - Table view data source
--- PlateWeight/ZHPoundsTableViewController.swift
+++ PlateWeight/ZHPoundsTableViewController.swift
@@ -12,21 +12,7 @@ class ZHPoundsTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    }
-    
-    override func prefersStatusBarHidden() -> Bool {
-        return true
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.navigationItem.title = "lbs to kgs";
     }
     
     // MARK: - Table view data source
